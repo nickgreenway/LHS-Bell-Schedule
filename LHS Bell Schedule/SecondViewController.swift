2522ef7da08a0291e9c5ab50a0541aad93011f4f
nickgreenway <nickgreenway@macbookpro.local>
2015-10-26 11:20:36 -0400

    Initial Commit

---
+++ LHS Bell Schedule/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LHS Bell Schedule
+//
+//  Created by Nick Greenway on 10/26/15.
+//  Copyright © 2015 Nick Greenway. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
